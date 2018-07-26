2e4eef632fba637b9674f0270760f4dda51a2fde
Rafael Thayto <rafa.thayto@gmail.com>
2018-07-26 00:24:26 +0000

    CRUD and image upload

---
+++ simple-backend/app/Controllers/Http/PropertyController.js
@@ -0,0 +1,92 @@
+'use strict'
+
+const Property = use('App/Models/Property')
+
+/**
+ * Resourceful controller for interacting with properties
+ */
+class PropertyController {
+  /**
+   * Show a list of all properties.
+   * GET properties
+   */
+  async index ({ request }) {
+    const { latitude, longitude } = request.all()
+
+    const properties = Property.query()
+      .with('images')
+      .nearBy(latitude, longitude, 10)
+      .fetch()
+
+    return properties
+  }
+
+  /**
+   * Create/save a new property.
+   * POST properties
+   */
+  async store ({ auth, request, response }) {
+    const { id } = auth.user
+    const data = request.only([
+      'title',
+      'address',
+      'latitude',
+      'longitude',
+      'price'
+    ])
+
+    const property = await Property.create({ ...data, user_id: id })
+
+    return property
+  }
+
+  /**
+   * Display a single property.
+   * GET properties/:id
+   */
+  async show ({ params }) {
+    const property = await Property.findOrFail(params.id)
+
+    await property.load('images')
+
+    return property
+  }
+
+  /**
+   * Update property details.
+   * PUT or PATCH properties/:id
+   */
+  async update ({ params, request, response }) {
+    const property = await Property.findOrFail(params.id)
+
+    const data = request.only([
+      'title',
+      'address',
+      'latitude',
+      'longitude',
+      'price'
+    ])
+
+    property.merge(data)
+
+    await property.save()
+
+    return property
+  }
+
+  /**
+   * Delete a property with id.
+   * DELETE properties/:id
+   */
+  async destroy ({ params, auth, response }) {
+    const property = await Property.findOrFail(params.id)
+
+    if (property.user_id !== auth.user.id) {
+      return response.status(401).send({ error: 'Not Authorized' })
+    }
+
+    await property.delete()
+  }
+}
+
+module.exports = PropertyController
--- simple-backend/start/routes.js
+++ simple-backend/start/routes.js
@@ -17,3 +17,12 @@ const Route = use('Route')
 
 Route.post('/users', 'UserController.create')
 Route.post('/sessions', 'SessionController.create')
+
+Route.resource('properties', 'PropertyController')
+  .apiOnly()
+  .middleware('auth')
+
+Route.post('properties/:id/images', 'ImageController.store')
+  .middleware('auth')
+
+Route.get('images/:path', 'ImageController.show')
